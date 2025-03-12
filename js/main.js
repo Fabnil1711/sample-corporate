2bb1ac631fe998108c1ebaae0ef1dbf01130c214
Fabnil1711 <fabnir2@gmail.com>
2025-03-12 17:37:33 +0900

    maybeComplete

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
-$(function () {
-    $('#header').on('click', function () {
-        alert('clicked')
+$(function()){
+    $('.hamburger').on('click',function(){
+        if ($('#header').hasClass('open')){
+            $('#header').removeClass('open');
+        } else {
+            $('#header').addClass('open');
+        }
     });
-});
+
+    $('#mask').on('click', function() {
+        $('header').removeClass('open');
+    });
+
+    $('#navi a').on('click', function() {
+        $('#header').removeClass('open')
+    });
+
+    let pagetop = $('#to-top');
+
+    pagetop.hide();
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 700) {
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    })
+
+    pagetop.click(function(){
+        $('body,html').animate({scrollTop: 0}, 500);
+        return false;
+    })
+}
